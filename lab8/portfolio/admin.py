81cb7fae3cc42c681434250f6a2dc45a0a5697d4
RicardoCleto22006526 <a22006526@alunos.ulht.pt>
2022-05-23 18:47:46 +0100

    PW Project ➔ Facilitou o admin

--- lab8/portfolio/admin.py
+++ lab8/portfolio/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 from .models import Quizz
+from .models import Pessoa
+from .models import Projetos
+from .models import Formacao
+
+class teste(admin.ModelAdmin):
+    filter_horizontal = ('participantes',)
 
 admin.site.register(Post)
 admin.site.register(Quizz)
+admin.site.register(Pessoa)
+admin.site.register(Projetos, teste)
+admin.site.register(Formacao)
+
+
+
